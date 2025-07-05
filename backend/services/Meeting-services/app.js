ffa544d8b70f9da56f4e900a3f07100d7c08a1e1
Abhi-8676 <kumarr.abhishekk812@gmail.com>
2025-07-05 11:17:25 +0530

    cors origin updates with https://meeting-scheduling-eight.vercel.app

--- backend/services/Meeting-services/app.js
+++ backend/services/Meeting-services/app.js
@@ -15,14 +15,14 @@ const server = http.createServer(app);
 // Configure Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://meeting-scheduling-eight.vercel.app",
     methods: ["GET", "POST"]
   },
   path: "/socket.io"
 });
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "https://meeting-scheduling-eight.vercel.app",
   credentials: true,
 }));
 app.use(express.json());
