9231098d94e1de8a7b4276b689c25c61a1a40d54
Abhi-8676 <kumarr.abhishekk812@gmail.com>
2025-08-31 21:45:51 +0530

    Update app.js

--- backend/gateway/app.js
+++ backend/gateway/app.js
@@ -5,7 +5,10 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 
